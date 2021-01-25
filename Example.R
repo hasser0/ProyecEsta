788d36d6f2c979374f60cdbb581660ec0e7f58f9
hasser0 <osvaldoes.unam@gmail.com>
2021-01-25 15:10:34 -0600

    Borramos cada Example.R

--- Example.R
+++
@@ -1,9 +0,0 @@
-cat_id = mexico_youtube_21_24_01$categoryId
-
-cat_data = aggregate(data.frame(count = cat_id),
-                     list(value = cat_id),
-                     length)
-barplot(height = cat_data[,2],names.arg = cat_data[,1])
-mexico_youtube_21_24_01$view_count
-cat_data
-getwd()
